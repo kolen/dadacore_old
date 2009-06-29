48707fad3c1e81a12e3ec3f0825cd5a27e21094e
kolen <incredible.angst@gmail.com>
2009-06-30 00:55:36 +0400

    Dirty logging in ddcweb.

--- ddcweb.py
+++ ddcweb.py
@@ -13,6 +13,8 @@ mmodel = ddc.MModel()
 brain = ddc.Brain(mmodel)
 brain.learn(u"Test test test.")
 
+brainlog = open("brain.log", "w")
+
 class index:
     def GET(self):
         randomlines = [ brain.generate_random() for i in range(1,10) ]
@@ -22,6 +24,7 @@ class index:
         input = web.input()
 
         for line in input.learntext.split("\n"):
+            brainlog.write("%s\n" % line.strip().encode('utf-8'))
             try:
                 brain.learn(line)
             except ddc.SequenceTooShortException:
